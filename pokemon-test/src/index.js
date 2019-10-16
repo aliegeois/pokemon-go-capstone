0e0c6d2e5de6cca53b7eb5d6df7d9cf0b6b33b26
aliegeois <pelanman@gmail.com>
2019-10-16 16:05:52 +0200

    début tman

--- pokemon-test/src/index.js
+++ pokemon-test/src/index.js
@@ -1,138 +1,89 @@
-import { paxos, template, target } from "foglet-template";
-import Foglet from 'foglet-core';
+// const { paxos, template, target } = require('foglet-template');
+const { Foglet } = require('foglet-core');
+const TMAN = require('../paxos-overlays/lib/overlay/overlay.js')
 
-class withFoglet {
-	state = {
-		nodes: [],
-		nodeData: [],
-		targets: [],
-		paxoses: {},
-		nodeTargets: {},
-		caught: {},
-		loading: false,
-		loadingMessage: "",
-		leaders: {},
-		nodeLeaders: {},
-		nodeIndex: 0,
-		targetIndex: 0
-	};
+/*navigator.geolocation.getCurrentPosition(async position => {
+	const x = position.coords.latitude;
+	const y = position.coords.longitude;
+	console.log('data:', {x, y});
+
+	new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: x,
+			lng: y
+		},
+		zoom: 18
+	});
+});*/
+
+const fog = new Foglet({
+	rps: {
+		type: 'cyclon',
+		options: {
+			protocol: 'my-awesome-broadcast-application', // the name of the protocol run by our app
+			webrtc: { // some WebRTC options
+				trickle: true, // enable trickle
+				iceServers : [] // define iceServers here if you want to run this code outside localhost
+			},
+			signaling: { // configure the signaling server
+				address: 'http://signaling.herokuapp.com', // put the URL of the signaling server here
+				room: 'my-awesome-broadcast-application' // the name of the room for the peers of our application
+			}
+		}
+	},
+	overlays: [{
+		name: 'tman',
+		class: TMAN,
+		options: {
+			protocol: 'my-awesome-broadcast-application',
+			signaling: {
+				address: 'http://signaling.herokuapp.com',
+				room: 'my-awesome-broadcast-application'
+			}
+		}
+	}]
+});
+
+// connect the foglet to the signaling server
+fog.share();
+
+// Connect the foglet to our network
+fog.connection().then(() => {
+	document.getElementById('id').innerHTML = fog.id;
+	// listen for broadcast messages
+	fog.onBroadcast((id, message) => {
+		console.log('The peer', id, 'just sent me by broadcast:', message);
+	});
 	
+	// send a message in broadcast
+	fog.sendBroadcast('Hello World !');
 
-	constructor() {
-		console.log('constructor');
-		
-		// super();
-		/*navigator.geolocation.getCurrentPosition(position => {
-			console.log(position.coords);
-			console.log('this', this);
-			withFoglet.addNode({
-				x: position.coords.longitude,
-				y: position.coords.latitude
-			});
-		});*/
+	// console.log('voisins', fog.overlay().network.getNeighbours());
+});
+
+document.getElementById('refresh').addEventListener('click', () => {
+	// document.getElementById('neighbours').innerHTML = fog.overlay().network.getNeighbours();
+	const n = document.getElementById('neighbours');
+	n.innerHTML = '';
+	const tr1 = document.createElement('tr');
+	for(let neighbour of fog.overlay().network.getNeighbours()) {
+		const td = document.createElement('td');
+		td.innerHTML = neighbour;
+		tr1.appendChild(td);
 	}
+	n.appendChild(tr1);
+	const tr2 = document.createElement('tr');
+	const overlayTman = fog.overlay('tman');
+	console.log(overlayTman);
+	if(!overlayTman)
+		return;
+	for(let neighbour of overlayTman.network.getNeighbours()) {
+		const td = document.createElement('td');
+		td.innerHTML = neighbour;
+		tr2.appendChild(td);
+	}
+	n.appendChild(tr2);
+});
 
-	test = () => {
-		console.log('dans test');
-	};
-	
-	addNode = data => async () => {
-		
-		navigator.geolocation.getCurrentPosition(async position => {
-			const x = position.coords.latitude;
-			const y = position.coords.longitude;
-			console.log('data:', {x, y});
-		
-			new google.maps.Map(document.getElementById('map'), {
-				center: {
-					lat: x,
-					lng: y
-				},
-				zoom: 18
-			});
-		
-			if (!x || !y) return;
-			const id = this.state.nodeIndex + 1;
 
-			
-			// const node = new template({ foglet: { id } }, true);
-			const node = new template({ foglet: { id } }, false);
-			console.log('overlays', node.getOverlays());
-			node.setDescriptor(data);
-			const rn = Math.floor(Math.random() * this.state.nodes.length);
-			const randomNode = this.state.nodes[rn];
-			await node.connection(randomNode);
-			
-			const nodeTargets = this.state.nodeTargets;
-			const targets = [];
-			
-			this.state.targets.forEach(target => {
-				const response = node.targetSpawned(target);
-				if (!response) return;
-				
-				targets.push({
-					...target.getCoordinates(),
-					id: target.id,
-					pokemon: target.pokemon
-				});
-			});
-			
-			const nodeId = node.foglet.inViewID;
-			nodeTargets[nodeId] = targets;
-			
-			const nodePaxos = new paxos(node, ({ cible, pid }) => {
-			});
-
-			
-			node.foglet.onUnicast(console.log);
-			node.sendUnicastAll('dbar');
-		});
-	};
-	
-	addTarget = data => () => {
-		const { x, y, pokemon, perimeter } = data;
-		if (!x || !y || !pokemon || !perimeter) return;
-		const id = this.state.targetIndex + 1;
-		const spawned = new target(id, {
-			coordinates: { x, y },
-			perimeter
-		});
-		
-		spawned.pokemon = pokemon;
-	};
-}
-
-const addMap = () => {
-    /*const map = new google.maps.Map(document.getElementById("map"), {
-        // Nous définissons le zoom par défaut
-        zoom: 11,
-        // Nous définissons le type de carte (ici carte routière)
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        // Nous activons les options de contrôle de la carte (plan, satellite...)
-        mapTypeControl: true,
-        // Nous désactivons la roulette de souris
-        scrollwheel: false,
-        mapTypeControlOptions: {
-            // Cette option sert à définir comment les options se placent
-            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
-        },
-        // Activation des options de navigation dans la carte (zoom...)
-        navigationControl: true,
-        navigationControlOptions: {
-            // Comment ces options doivent-elles s'afficher
-            style: google.maps.NavigationControlStyle.ZOOM_PAN
-        }
-    });*/
-};
-
-let main = () => {
-	console.log('main');
-	const f = new withFoglet();
-	f.addNode()();
-};
-
-window.onload = function () {
-    // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-    addMap();
-};
-main();
+window.fog = fog;
