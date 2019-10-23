131a55536997a4a8576ec12f4b3ebd9369d36390
aliegeois <pelanman@gmail.com>
2019-10-23 13:06:51 +0200

    rafraichissement périodique de la position gps

--- pokemon-test/paxos-overlays/lib/overlay/overlay.js
+++ pokemon-test/paxos-overlays/lib/overlay/overlay.js
@@ -4,107 +4,108 @@ const MUpdateCache = require("./messages/mupdatecache.js");
 const MUpdatePartialView = require("./messages/mupdatepartialview.js");
 
 module.exports = class Overlay extends TMAN {
-  constructor(...Args) {
-    super(...Args);
-    debug("Overlay initialized");
-    this.rps._partialViewSize = () => this.options.maxPeers;
-    this.rps._sampleSize = () => this.options.maxPeers;
-    this._setupListener();
-    this._updateCache();
-  }
+	constructor(...Args) {
+		super(...Args);
+		debug("Overlay initialized");
+		this.rps._partialViewSize = () => this.options.maxPeers;
+		this.rps._sampleSize = () => this.options.maxPeers;
+		this._setupListener();
+		this._updateCache();
+	}
+	
+	/**
+	* Gives the start descriptor used by the TMan overlay (can be an empty object).
+	* Subclasses of {@link TManOverlay} **must** implement this method.
+	* @return {Object} The start descriptor used by the TMan overlay
+	*/
+	_startDescriptor() {
+		// return {
+		// 	id: "test",
+		// 	x: Math.floor(Math.random() * 10),
+		// 	y: Math.floor(Math.random() * 10),
+		// 	z: Math.floor(Math.random() * 50)
+		// };
+		return this.options.position;
+	}
+	
+	_setupListener(delay = this.options.delta) {
+		const overlay = this.options.pid;
+		setTimeout(() => {
+			this._manager.overlay(overlay).communication.onUnicast((id, message) => {
+				if (message.type === "MUpdateCache") {
+					this._rps.cache.set(message.peer, message.descriptor);
+					if (this._rps.partialView.has(message.peer)) {
+						const myDescriptor = this._rps.partialView.get(message.peer)
+						.descriptor;
+						myDescriptor.x = message.descriptor.x;
+						myDescriptor.y = message.descriptor.y;
+						myDescriptor.z = message.descriptor.z;
+					}
+				}
+			});
+		}, 0.5 * 1000);
+	}
+	
+	_updateCache(delay = this.options.delta) {
+		this.periodic = setInterval(() => {
+			const overlay = this.options.pid;
+			this._rps.parent.getPeers().forEach(peerId => {
+				this._manager
+				.overlay(overlay) && this._manager
+				.overlay(overlay)
+				.communication.sendUnicast(
+					peerId,
+					new MUpdateCache(this.inviewId, this._rps.options.descriptor)
+				).then().catch(e => {});
+			});
+		}, 2 * 1000);
+	}
 
-  /**
-   * Gives the start descriptor used by the TMan overlay (can be an empty object).
-   * Subclasses of {@link TManOverlay} **must** implement this method.
-   * @return {Object} The start descriptor used by the TMan overlay
-   */
-  _startDescriptor() {
-    return {
-      id: "test",
-      x: Math.floor(Math.random() * 10),
-      y: Math.floor(Math.random() * 10),
-      z: Math.floor(Math.random() * 50)
-    };
-  }
-
-  _setupListener(delay = this.options.delta) {
-    const overlay = this.options.pid;
-    setTimeout(() => {
-      this._manager.overlay(overlay).communication.onUnicast((id, message) => {
-        if (message.type === "MUpdateCache") {
-          this._rps.cache.set(message.peer, message.descriptor);
-          if (this._rps.partialView.has(message.peer)) {
-            const myDescriptor = this._rps.partialView.get(message.peer)
-              .descriptor;
-            myDescriptor.x = message.descriptor.x;
-            myDescriptor.y = message.descriptor.y;
-            myDescriptor.z = message.descriptor.z;
-          }
-        }
-      });
-    }, 0.5 * 1000);
-  }
-
-  _updateCache(delay = this.options.delta) {
-    this.periodic = setInterval(() => {
-      const overlay = this.options.pid;
-      this._rps.parent.getPeers().forEach(peerId => {
-          this._manager
-          .overlay(overlay) && this._manager
-          .overlay(overlay)
-          .communication.sendUnicast(
-            peerId,
-            new MUpdateCache(this.inviewId, this._rps.options.descriptor)
-          ).then().catch(
-            e => {
-
-            }
-          );
-      });
-    }, 2 * 1000);
-  }
-
-  /**
-   * Give the delay **in milliseconds** after which the descriptor must be recomputed.
-   * Subclasses of {@link TManOverlay} **must** implement this method.
-   * @return {number} The delay **in milliseconds** after which the descriptor must be recomputed
-   */
-  _descriptorTimeout() {
-    return 5 * 1000;
-  }
-
-  /**
-   * Compare two peers and rank them according to a ranking function.
-   * This function must return `0 if peerA == peerB`, `1 if peerA < peerB` and `-1 if peerA > peerB`.
-   *
-   * Subclasses of {@link TManOverlay} **must** implement this method.
-   * @param {*} neighbour - The neighbour to rank with
-   * @param {Object} descriptorA - Descriptor of the first peer
-   * @param {Object} descriptorB - Descriptor of the second peer
-   * @param {TManOverlay} peerA - (optional) The overlay of the first peer
-   * @param {TManOverlay} peerB - (optional) The overlay of the second peer
-   * @return {integer} `0 if peerA == peerB`, `1 if peerA < peerB` and `-1 if peerA > peerB` (according to the ranking algorithm)
-   */
-  _rankPeers(neighbour, descriptorA, descriptorB, peerA, peerB) {
-    const getDistance = (descriptor1, descriptor2) => {
-      const { x: xa, y: ya, z: za } = descriptor1;
-      const { x: xb, y: yb, z: zb } = descriptor2;
-      const dx = xa - xb;
-      const dy = ya - yb;
-      const dz = za - zb;
-      return Math.sqrt(dx * dx + dy * dy);
-      //return Math.sqrt(dx * dx + dy * dy + dz * dz);
-    };
-    const distanceA = getDistance(neighbour.descriptor, descriptorA);
-    const distanceB = getDistance(neighbour.descriptor, descriptorB);
-
-    if (distanceA === distanceB) {
-      if (descriptorA.x >= descriptorB.x) {
-        return -1;
-      } else if (descriptorA.x < descriptorB.x) {
-        return 1;
-      }
-    }
-    return distanceA - distanceB;
-  }
+	updateDescriptor(pos = {x: 0, y: 0}) {
+		this._rps.options.descriptor = pos;
+	}
+	
+	/**
+	* Give the delay **in milliseconds** after which the descriptor must be recomputed.
+	* Subclasses of {@link TManOverlay} **must** implement this method.
+	* @return {number} The delay **in milliseconds** after which the descriptor must be recomputed
+	*/
+	_descriptorTimeout() {
+		return 5 * 1000;
+	}
+	
+	/**
+	* Compare two peers and rank them according to a ranking function.
+	* This function must return `0 if peerA == peerB`, `1 if peerA < peerB` and `-1 if peerA > peerB`.
+	*
+	* Subclasses of {@link TManOverlay} **must** implement this method.
+	* @param {*} neighbour - The neighbour to rank with
+	* @param {Object} descriptorA - Descriptor of the first peer
+	* @param {Object} descriptorB - Descriptor of the second peer
+	* @param {TManOverlay} peerA - (optional) The overlay of the first peer
+	* @param {TManOverlay} peerB - (optional) The overlay of the second peer
+	* @return {integer} `0 if peerA == peerB`, `1 if peerA < peerB` and `-1 if peerA > peerB` (according to the ranking algorithm)
+	*/
+	_rankPeers(neighbour, descriptorA, descriptorB, peerA, peerB) {
+		const getDistance = (descriptor1, descriptor2) => {
+			const { x: xa, y: ya/*, z: za */} = descriptor1;
+			const { x: xb, y: yb/*, z: zb */} = descriptor2;
+			const dx = xa - xb;
+			const dy = ya - yb;
+			// const dz = za - zb;
+			return Math.sqrt(dx * dx + dy * dy);
+			//return Math.sqrt(dx * dx + dy * dy + dz * dz);
+		};
+		const distanceA = getDistance(neighbour.descriptor, descriptorA);
+		const distanceB = getDistance(neighbour.descriptor, descriptorB);
+		
+		if (distanceA === distanceB) {
+			if (descriptorA.x >= descriptorB.x) {
+				return -1;
+			} else if (descriptorA.x < descriptorB.x) {
+				return 1;
+			}
+		}
+		return distanceA - distanceB;
+	}
 };
--- pokemon-test/src/index.js
+++ pokemon-test/src/index.js
@@ -1,26 +1,40 @@
 // const { paxos, template, target } = require('foglet-template');
 const { Foglet } = require('foglet-core');
-const TMAN = require('../paxos-overlays/lib/overlay/overlay.js');
 const fetch = require('node-fetch');
+const TMAN = require('../paxos-overlays/lib/overlay/overlay.js');
 
-/*navigator.geolocation.getCurrentPosition(async position => {
-	const x = position.coords.latitude;
-	const y = position.coords.longitude;
-	console.log('data:', {x, y});
-
-	new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: x,
-			lng: y
-		},
-		zoom: 18
+let getCurrentPosition = (pos = {x: null, y: null}) => {
+	return new Promise((resolve, reject) => {
+		if(pos.x !== null && pos.y !== null) {
+			resolve(pos);
+		} else {
+			navigator.geolocation.getCurrentPosition(position => {
+				resolve({
+					x: position.coords.latitude,
+					y: position.coords.longitude
+				});
+			}, reject);
+		}
 	});
-});*/
+}
+
+getCurrentPosition().then(position => {
+	start(position);
+
+	setInterval(async () => {
+		updateCurrentPosition(await getCurrentPosition());
+	}, 5 * 1000);
+});
 
 let fog;
 
+let updateCurrentPosition = pos => {
+	fog.overlay('tman')._network._rps.options.descriptor = pos;
+};
+
 let refresh = () => {
 	const n = document.getElementById('neighbours');
+	const p = document.getElementById('pos');
 	n.innerHTML = '';
 	const tr1 = document.createElement('tr');
 	for(let neighbour of fog.overlay().network.getNeighbours()) {
@@ -40,81 +54,87 @@ let refresh = () => {
 		tr2.appendChild(td);
 	}
 	n.appendChild(tr2);
+
+	let d = fog.overlay('tman')._network._rps.options.descriptor;
+	p.innerHTML = `My position: (x: ${d.x}, y: ${d.y})`;
 };
 
-fetch('https://signaling.herokuapp.com/ice').then(data => data.json()).then(data => {
-	// console.log(data.ice.map(e => {return {...e, urls: e.url}}));
-	let data2 = data.ice.map(e => {
-		let e2 = {
-			...e,
-			urls: e.url
-		};
-		delete e2.url;
-		return e2;
-	});
-	// console.log(data2);
-	fog = new Foglet({
-		rps: {
-			type: 'cyclon',
-			options: {
-				delta: 10 * 1000,
-				timeout: 10 * 1000,
-				pendingTimeout: 30 * 1000,
-				protocol: 'pokestone', // the name of the protocol run by our app
-				webrtc: { // some WebRTC options
-					trickle: true, // enable trickle
-					// iceServers : data.ice, // define iceServers here if you want to run this code outside localhost
-					config: {
-						iceServers: data2
+let start = position => {
+	fetch('https://signaling.herokuapp.com/ice').then(data => data.json()).then(data => {
+		// console.log(data.ice.map(e => {return {...e, urls: e.url}}));
+		let data2 = data.ice.map(e => {
+			let e2 = {
+				...e,
+				urls: e.url
+			};
+			delete e2.url;
+			return e2;
+		});
+		// console.log(data2);
+		fog = new Foglet({
+			rps: {
+				type: 'cyclon',
+				options: {
+					delta: 10 * 1000,
+					timeout: 10 * 1000,
+					pendingTimeout: 30 * 1000,
+					protocol: 'pokestone', // the name of the protocol run by our app
+					webrtc: { // some WebRTC options
+						trickle: true, // enable trickle
+						// iceServers : data.ice, // define iceServers here if you want to run this code outside localhost
+						config: {
+							iceServers: data2
+						}
+					},
+					signaling: { // configure the signaling server
+						address: 'https://signaling.herokuapp.com', // put the URL of the signaling server here
+						room: 'pokestone' // the name of the room for the peers of our application
 					}
-				},
-				signaling: { // configure the signaling server
-					address: 'https://signaling.herokuapp.com', // put the URL of the signaling server here
-					room: 'pokestone' // the name of the room for the peers of our application
 				}
-			}
-		},
-		overlays: [{
-			name: 'tman',
-			class: TMAN,
-			options: {
-				delta: 10 * 1000,
-				timeout: 10 * 1000,
-				pendingTimeout: 10 * 1000,
-				maxPeers: 10,
-				protocol: 'pokestone',
-				signaling: {
-					address: 'https://signaling.herokuapp.com',
-					room: 'pokestone'
+			},
+			overlays: [{
+				name: 'tman',
+				class: TMAN,
+				options: {
+					delta: 10 * 1000,
+					timeout: 10 * 1000,
+					pendingTimeout: 10 * 1000,
+					maxPeers: 10,
+					protocol: 'pokestone',
+					signaling: {
+						address: 'https://signaling.herokuapp.com',
+						room: 'pokestone'
+					},
+					position
 				}
-			}
-		}]
-	});
-	
-	// connect the foglet to the signaling server
-	fog.share();
-	
-	// Connect the foglet to our network
-	fog.connection().then(() => {
-		fog.overlay('tman').network.rps._start();
-		document.getElementById('id').innerHTML = fog.id;
-		// listen for broadcast messages
-		fog.onBroadcast((id, message) => {
-			console.log('The peer', id, 'just sent me by broadcast:', message);
+			}]
 		});
 		
-		// send a message in broadcast
-		// fog.sendBroadcast('Hello World !');
-	
-		fog.overlay().network.rps.on('open', refresh);
-		fog.overlay().network.rps.on('close', refresh);
-		fog.overlay('tman').network.rps.on('open', refresh);
-		fog.overlay('tman').network.rps.on('close', refresh);
-		// console.log('voisins', fog.overlay().network.getNeighbours());
+		// connect the foglet to the signaling server
+		fog.share();
+		
+		// Connect the foglet to our network
+		fog.connection().then(() => {
+			fog.overlay('tman').network.rps._start();
+			document.getElementById('id').innerHTML = fog.id;
+			// listen for broadcast messages
+			fog.onBroadcast((id, message) => {
+				console.log('The peer', id, 'just sent me by broadcast:', message);
+			});
+
+			// console.log('rps', fog.overlay().network.rps);
+			
+			// send a message in broadcast
+			// fog.sendBroadcast('Hello World !');
+		
+			fog.overlay().network.rps.on('open', refresh);
+			fog.overlay().network.rps.on('close', refresh);
+			fog.overlay('tman').network.rps.on('open', refresh);
+			fog.overlay('tman').network.rps.on('close', refresh);
+			// console.log('voisins', fog.overlay().network.getNeighbours());
+		});
+		
+		
+		window.fog = fog;
 	});
-	
-	document.getElementById('refresh').addEventListener('click', refresh);
-	
-	
-	window.fog = fog;
-});
+};
