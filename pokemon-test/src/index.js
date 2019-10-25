d32c5f5f2575c48ac6723167d24b64f204a99e18
Kairides <gueguen.ronan1@gmail.com>
2019-10-25 11:14:24 +0200

    Ajout de la carte

--- pokemon-test/src/index.js
+++ pokemon-test/src/index.js
@@ -3,6 +3,293 @@ const { Foglet } = require('foglet-core');
 const fetch = require('node-fetch');
 const TMAN = require('../paxos-overlays/lib/overlay/overlay.js');
 
+navigator.geolocation.getCurrentPosition(async position => {
+	const x = position.coords.latitude;
+	const y = position.coords.longitude;
+	var map;
+	console.log('data:', {x, y});
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {
+			lat: x,
+			lng: y
+		},
+		zoom: 19
+	});
+	
+	map.setOptions({styles: [
+
+		{
+			featureType: 'water',
+			stylers: [{color:'#00C6D8'}]
+		},
+		{
+			featureType: 'road.local',
+			stylers: [{color: '#E4C52A'}]
+		},
+		{
+			featureType: 'landscape.natural',
+			stylers: [{color:'#7CE748'}]
+		},
+		{
+			featureType: 'landscape.natural.terrain',
+			stylers: [{color:'#5BD529'}]
+		},
+		{
+			featureType: 'administrative.land_parcel',
+			stylers: [{color:'#FC5061'}]
+		},
+		{
+			featureType: 'all',
+			elementType: 'labels.icon',
+			stylers: [{visibility: 'off'}]
+		},
+		{
+			featureType: 'all',
+			elementType: 'labels.text',
+			stylers: [{visibility:'off'}]
+		}
+		/*[
+  {
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#fc5061"
+      },
+      {
+        "visibility": "on"
+      },
+      {
+        "weight": 2.5
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#5bd529"
+      },
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural.landcover",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#369100"
+      },
+      {
+        "weight": 8
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural.landcover",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "weight": 6.5
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "weight": 8
+      }
+    ]
+  },
+  {
+    "featureType": "poi.attraction",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#fc5061"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.government",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#fc5061"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#7ce748"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.place_of_worship",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#fc5061"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.school",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "weight": 8
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f7c510"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#0098b5"
+      }
+    ]
+  }
+]*/
+	]});
+
+	var icons = {
+		pokeball:{
+			//TODO: faire nos propres icones
+			icon:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnJW8QWOGW4h8cREWdx7gU352re88-07fAQsyD5r6ekEH6SWnSbg&s'
+		}
+	};
+
+	/*var features = [
+		{
+			position: {lat:x, lng:y}, 
+			type: 'pokeball'
+		}
+	];*/
+
+	for (var i = 0; i < features.length; i++){
+		var marker = new google.maps.Marker({
+			position:features[i].position,
+			icon: icons[features[i].type].icon,
+			map: map
+		});
+	};
+});
+
+
 let getCurrentPosition = (pos = {x: null, y: null}) => {
 	return new Promise((resolve, reject) => {
 		if(pos.x !== null && pos.y !== null) {
