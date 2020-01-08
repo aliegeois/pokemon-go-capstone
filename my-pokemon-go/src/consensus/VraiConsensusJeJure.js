6e90e9d5084f3a3393284b0c63fd6368a748c6c4
aliegeois <pelanman@gmail.com>
2020-01-08 13:37:06 +0100

    consensus qui marche, mashallah

---
+++ my-pokemon-go/src/consensus/VraiConsensusJeJure.js
@@ -0,0 +1,25 @@
+export default class Consensus {
+	/**
+	 * @param {Network} overlay 
+	 * @param {Pokemon} pokemon 
+	 * @param {function(string, {x: number, y: number, peer: string}): void} onLeader 
+	 */
+	constructor(overlay, pokemon, onLeader) {
+		console.log('consensus', this);
+		/** @private */
+		this._overlay = overlay;
+		/** @private */
+		this._pokemon = pokemon;
+	}
+
+	get pokemon() {
+		return this._pokemon;
+	}
+
+	/**
+	 * @return {Promise}
+	 */
+	catch() {
+		return Promise.resolve(this._overlay.network.rps.getInviewId())
+	}
+}
